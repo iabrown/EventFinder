69e9b32401568b429463293bfbdab4102a27eb75
iabrown <ian@iabdesigns.com>
2020-12-08 12:55:14 -0800

    Modified SearchBar file to have controlled data and including state.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,15 +2,15 @@ import '../css/AppStylesheet.css';
 import React from 'react';
 
 class SearchBar extends React.Component{
-  onInputChange(event){
-    console.log(event.target.value)
-  }
+  state = {term: ''};
+
+
   render(){
     return(
      <div className="container-fluid SearchBar">
-      <form>
+      <form onSubmit={this.onInputChange}>
       <label> Event Search...</label>
-      <input type="text" onChange={this.onInputChange}/>
+      <input type="text" id="SearchBar" value={this.state.term} onChange={e => this.setState({term: e.target.value})} />
       </form>
      </div>
    );
