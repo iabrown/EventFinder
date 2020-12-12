047ef6870c64e106a54c3606c3ebc889ad843a08
iabrown <ian@iabdesigns.com>
2020-12-12 00:42:37 -0800

    Added CSS styling to the footer component. Added the Font Family of Roboto to the Google Font CDN.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 class Footer extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      year:"2020"
+    }
+  }
+
+
+
+
   render(){
     return(
       <div className="container-fluid Footer">
+      <h1>Copyright &copy; {this.state.year} IAB Designs LLC. Theme designed by Bengs One</h1>
+      <p> Connect on Instagram & Twitter: @Bengs_one</p>
 
       </div>
     );
